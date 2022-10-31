a24c68eff2479fd8bdf5159abd103800ae05843c
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2022-10-31 23:19:14 +0300

    Time: 30 ms (15.02%), Space: 13.6 MB (72.96%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.swift
+++ 0509-fibonacci-number/0509-fibonacci-number.swift
@@ -1,6 +1,7 @@
 class Solution {
     func fib(_ n: Int) -> Int {
-        if(n==0 || n==1) {return n;}
+        if(n==0) {return 0;}
+        if(n==1) {return 1;}
         return fib(n-1)+fib(n-2);
     }
 }
