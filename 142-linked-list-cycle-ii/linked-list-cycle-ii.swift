579749ce2d244e913450c9e666aa03d07a4f3420
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2025-11-28 22:45:23 +0200

    Time: 16 ms (89.02%) | Memory: 18.1 MB (52.44%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        var slow = head
+        var fast = head
+        while slow != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+            if slow === fast
+            {
+                slow = head
+                while slow !== fast 
+                {
+                    slow = slow?.next
+                    fast = fast?.next
+                }
+                return slow
+            } 
+        }
+        return nil
+    }
+}
