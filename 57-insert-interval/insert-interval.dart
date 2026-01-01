bc3b80c2dd40cadac549cedd2dd9484865edae34
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-01 21:43:35 +0200

    Time: 4 ms (25.00%) | Memory: 151.8 MB (50.00%) - LeetSync

---
+++ 57-insert-interval/insert-interval.dart
@@ -0,0 +1,47 @@
+class Solution {
+  List<List<int>> insert(List<List<int>> intervals, List<int> newInterval) {
+    if(intervals.length>0)
+    {
+        final List<List<int>> newList = [];
+        List<int>? nullableInterval = newInterval;
+        for(int i=0;i<intervals.length;i++){
+            final currentInterval = intervals[i];
+            if(nullableInterval != null){
+            if(nullableInterval[0]<= currentInterval[1])
+            {
+                if(nullableInterval[0]>= currentInterval[0]){
+                    newList.add([currentInterval[0],currentInterval[1]>nullableInterval[1]?currentInterval[1]:nullableInterval[1]]);
+                }else{
+
+                    if( nullableInterval[1]>=currentInterval[0]){
+                    newList.add([nullableInterval[0],currentInterval[1]>nullableInterval[1]?currentInterval[1]:nullableInterval[1]]);
+                    }else{
+                    newList.add(nullableInterval);
+                    newList.add(currentInterval);
+                    }
+                }
+                nullableInterval = null;
+                
+            }else{
+                newList.add(currentInterval);
+            }
+            }else{
+                List<int> lastInterval = newList.last;
+                if(currentInterval[0]<=lastInterval[1])
+                {
+                    newList[newList.length-1] = [lastInterval[0],currentInterval[1]>lastInterval[1]?currentInterval[1]:lastInterval[1]];
+                }else{
+                    newList.add(currentInterval);
+                }
+            }
+
+        }
+        if(nullableInterval != null){
+            newList.add(nullableInterval);
+        }
+        return newList;
+    }else{
+        return [newInterval];
+    }
+  }
+}
