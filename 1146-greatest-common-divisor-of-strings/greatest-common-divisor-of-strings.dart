6f0285bc82fa50c15d85e426f5feb925ea4f1261
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-06 20:16:00 +0200

    Time: 0 ms (100.00%) | Memory: 149.1 MB (53.33%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.dart
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.dart
@@ -1,12 +1,12 @@
 class Solution {
   String gcdOfStrings(String str1, String str2) {
-    String small = str1.length<str2.length?str1:str2;
-    for(int i=small.length;i>0;i--){
-       String qcd = small.substring(0,i);
-        if(str1.replaceAll(qcd,'').isEmpty && str2.replaceAll(qcd,'').isEmpty){
-            return qcd;
-        }
+    if(str1 + str2 != str2 + str1) return '';
+
+    int qcd(int a, b){
+        if(b == 0) return a;
+        return qcd(b,a % b);
     }
-    return '';
+
+    return str1.substring(0,qcd(str1.length,str2.length));
   }
 }
