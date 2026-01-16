246aa8d43f8f10510831742cdab49ee75d97fc0d
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-16 23:22:38 +0200

    Time: 35 ms (15.38%) | Memory: 149.6 MB (61.54%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.dart
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.dart
@@ -1,19 +1,19 @@
 class Solution {
   int maxVowels(String s, int k) {
     int maxNVowels=0;
-    RegExp vowels = RegExp(r'[aeiou]');
+    // RegExp vowels = RegExp(r'[aeiou]');
     for(int i=0;i<k;i++){
-        if(vowels.hasMatch(s[i])){
+        if('aeiou'.contains(s[i])){
             maxNVowels++;
         }
     }
 
 int max = maxNVowels;
     for(int i=k;i<s.length;i++){
-        if(vowels.hasMatch(s[i])){
+        if('aeiou'.contains(s[i])){
             max++;
         }
-        if(vowels.hasMatch(s[i-k])){
+        if('aeiou'.contains(s[i-k])){
             max--;
         }
         if(max>maxNVowels){
