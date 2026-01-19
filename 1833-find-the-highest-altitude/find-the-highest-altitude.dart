9f16f1617f8e47cc154fa46022142261089adcc8
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-19 23:16:50 +0200

    Time: 0 ms (100.00%) | Memory: 146.9 MB (96.77%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.dart
@@ -0,0 +1,14 @@
+class Solution {
+  int largestAltitude(List<int> gain) {
+    int max = 0;
+    int sum = 0;
+    for(int i=0;i<gain.length;i++){
+        sum +=gain[i];
+        if(sum>max){
+            max=sum;
+        }
+    }
+    return max;
+    
+  }
+}
