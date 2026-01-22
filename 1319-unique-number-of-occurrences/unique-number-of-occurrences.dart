4defbb4b4facaa7090582969916a78a51f4fd22f
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-22 22:33:03 +0200

    Time: 6 ms (76.47%) | Memory: 148.6 MB (76.47%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.dart
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.dart
@@ -4,7 +4,16 @@ class Solution {
     for(int i=0;i<arr.length;i++){
         result[arr[i]] = (result[arr[i]] ?? 0) + 1;
     }
-    List list = result.entries.map((k)=>k.value).toList();
-    return list.length == list.toSet().length;
+    // List list = result.entries.map((k)=>k.value).toList();
+    // return list.length == list.toSet().length;
+    Set<int> seen = {};
+    for( int count in result.values){
+        if(seen.contains(count) ){
+            return false;
+        }else{
+            seen.add(count);
+        }
+    }
+    return true;
   }
 }
