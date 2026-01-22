cc140a45eec174361ce2b32117a05b2c6695e834
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-22 22:14:13 +0200

    Time: 50 ms (36.36%) | Memory: 153.4 MB (9.09%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.dart
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.dart
@@ -1,18 +1,20 @@
 class Solution {
   List<List<int>> findDifference(List<int> nums1, List<int> nums2) {
-    List<int> n1 = [];
-    List<int> n2 = [];
-    for(int i=0;i<nums1.length;i++){
-        if(nums2.contains(nums1[i]) == false && n1.contains(nums1[i]) == false){
-            n1.add(nums1[i]);
-        }
-    }
-    for(int i=0;i<nums2.length;i++){
-        if(nums1.contains(nums2[i])==false && n2.contains(nums2[i]) == false){
-            n2.add(nums2[i]);
-        }
-    }
+    // List<int> n1 = [];
+    // List<int> n2 = [];
+    // for(int i=0;i<nums1.length;i++){
+    //     if(nums2.contains(nums1[i]) == false && n1.contains(nums1[i]) == false){
+    //         n1.add(nums1[i]);
+    //     }
+    // }
+    // for(int i=0;i<nums2.length;i++){
+    //     if(nums1.contains(nums2[i])==false && n2.contains(nums2[i]) == false){
+    //         n2.add(nums2[i]);
+    //     }
+    // }
 
-    return [n1,n2];
+    // return [n1,n2];
+
+    return [nums1.toSet().difference(nums2.toSet()).toList(),nums2.toSet().difference(nums1.toSet()).toList()];
   }
 }
