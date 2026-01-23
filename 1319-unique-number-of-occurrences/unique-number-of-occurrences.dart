c549b04121b15bfaea1d21950278445f1e4dcf77
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-23 23:52:56 +0200

    Time: 5 ms (88.24%) | Memory: 149.8 MB (52.94%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.dart
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.dart
@@ -4,8 +4,6 @@ class Solution {
     for(int i=0;i<arr.length;i++){
         result[arr[i]] = (result[arr[i]] ?? 0) + 1;
     }
-    // List list = result.entries.map((k)=>k.value).toList();
-    // return list.length == list.toSet().length;
     Set<int> seen = {};
     for( int count in result.values){
         if(seen.contains(count) ){
