72eb28c6e4c4a3b2f5b078178ccf55a0b7824271
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-24 23:42:01 +0200

    Time: 123 ms (6.06%) | Memory: 158.5 MB (87.88%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.dart
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.dart
@@ -1,15 +1,16 @@
 class Solution {
   int equalPairs(List<List<int>> grid) {
     int count =0;
-    for(int i=0;i<grid.length;i++){
-        for(int j=0;j<grid.length;j++){
-            int horCount = 0;
-            for(int k = 0;k<grid[i].length;k++){
-                if(grid[i][k]==grid[k][j]){
-                horCount++;
+    int n =grid.length;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            bool isEqual = true;
+            for(int k = 0;k<n;k++){
+                if(grid[i][k]!=grid[k][j]){
+                isEqual = false;
                 }
             }
-            if(horCount == grid[j].length){
+            if(isEqual){
                 count++;
             }
         }
