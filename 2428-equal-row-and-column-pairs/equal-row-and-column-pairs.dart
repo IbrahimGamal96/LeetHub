e4746012f87bf322ced2c4ecf14ff9513a43df38
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-01-25 00:01:37 +0200

    Time: 60 ms (93.94%) | Memory: 159.8 MB (69.70%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.dart
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.dart
@@ -1,46 +1,29 @@
 class Solution {
-  List<List<int>> transpose(List<List<int>> gr) {
-    final List<List<int>> gc = [].map((e) => <int>[]).toList();
-    int n = gr.length;
-
-    for (int i = 0; i < n; i++) {
-      final List<int> v = <int>[];
-      for (int j = 0; j < n; j++) {
-        v.add(gr[j][i]); // column-wise pushing
-      }
-      gc.add(v);
-    }
-
-    return gc;
-  }
-
-  bool isListEqual(List<int> listOne, List<int> listTwo) {
-    if (listOne.length != listTwo.length) {
-      return false;
-    }
-
-    for (int i = 0; i < listOne.length; i++) {
-      if (listOne[i] != listTwo[i]) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  int equalPairs(List<List<int>> gr) {
-    final List<List<int>> gc = transpose(gr);
-    final int n = gr.length;
-
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (isListEqual(gr[i], gc[j])) {
-          ans++;
+  int equalPairs(List<List<int>> grid) {
+    int count =0;
+    int n =grid.length;
+    List<List<int>> gridCo = [];
+    for(int i=0;i<n;i++){
+        List<int> list = [];
+        for(int j=0;j<n;j++){
+            list.add(grid[j][i]);
         }
-      }
+        gridCo.add(list);
     }
-
-    return ans;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            bool isEqual = true;
+            for(int k = 0;k<n;k++){
+                if(grid[i][k]!=gridCo[j][k]){
+                isEqual = false;
+                }
+            }
+            if(isEqual){
+                count++;
+            }
+        }
+    }
+    return count;
   }
 }
