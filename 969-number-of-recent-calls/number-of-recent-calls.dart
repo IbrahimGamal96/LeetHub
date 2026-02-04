6a4e044881ec993ad67be2034e0d57e0838cd353
IbrahimGamal96 <102231110+IbrahimGamal96@users.noreply.github.com>
2026-02-04 23:46:11 +0200

    Time: 264 ms (57.14%) | Memory: 190.2 MB (21.43%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.dart
@@ -0,0 +1,24 @@
+class RecentCounter {
+late List<int> nums;
+  RecentCounter() {
+    nums = [];
+  }
+  
+  int ping(int t) {
+    nums.add(t);
+    int count = 0;
+    int f = t-3000;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]>=f){
+            count++;
+        }
+    }
+    return count;
+  }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = RecentCounter();
+ * int param1 = obj.ping(t);
+ */
